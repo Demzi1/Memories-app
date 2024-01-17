fc62380333472fdd66ce88d2033fefcbf1c42379
Demzi1 <dembojatta133@gmail.com>
2024-01-17 02:25:12 +0000

    added build

---
+++ client/build/precache-manifest.826c98861067b6539fc8901a0f3558bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c447b98c00e3aa695273ff5c5f5fde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab636ac983428b265882",
+    "url": "/static/css/main.25336470.chunk.css"
+  },
+  {
+    "revision": "d92a5cdeee0c09cd3786",
+    "url": "/static/js/2.e6d34604.chunk.js"
+  },
+  {
+    "revision": "3ee643f80d517722512657fa5e3c3147",
+    "url": "/static/js/2.e6d34604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab636ac983428b265882",
+    "url": "/static/js/main.ae5a57de.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ae5a57de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/static/media/memories-Logo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/static/media/memories-Text.6c7b5ec0.png"
+  }
+]);
